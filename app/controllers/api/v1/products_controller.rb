be8921815ce5af8ea11f7bbd750e8bbfedc6847d
Oblum1989 <oscar.blum@gmail.com>
2019-05-02 20:56:06 -0500

    Listar productos con JSON-1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
-    render json: @products
+    format.json {render json: @products}
   end
 
   def new
